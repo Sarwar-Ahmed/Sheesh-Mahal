8228ef9298d75676be658c7bacd1fd37f33d5589
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2023-04-29 23:13:53 +0100

    Cart Ui Design Done

---
+++ app/src/main/java/com/example/sheeshmahal/adapters/CartAdapter.java
@@ -0,0 +1,62 @@
+package com.example.sheeshmahal.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sheeshmahal.R;
+import com.example.sheeshmahal.models.CartModel;
+
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
+
+    List<CartModel> list;
+
+    public CartAdapter(List<CartModel> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.imageView.setImageResource(list.get(position).getImage());
+        holder.name.setText(list.get(position).getName());
+        holder.rating.setText(list.get(position).getRating());
+        holder.price.setText(list.get(position).getPrice());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name,rating,price;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.cart_img);
+            name = itemView.findViewById(R.id.cart_item_name);
+            rating = itemView.findViewById(R.id.cart_rating);
+            price = itemView.findViewById(R.id.cart_item_price);
+
+        }
+    }
+}
--- app/src/main/java/com/example/sheeshmahal/ui/CartFragment.java
+++ app/src/main/java/com/example/sheeshmahal/ui/CartFragment.java
@@ -3,64 +3,50 @@ package com.example.sheeshmahal.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sheeshmahal.R;
+import com.example.sheeshmahal.adapters.CartAdapter;
+import com.example.sheeshmahal.models.CartModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    List<CartModel> list;
+    CartAdapter cartAdapter;
+    RecyclerView recyclerView;
 
     public CartFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CartFragment newInstance(String param1, String param2) {
-        CartFragment fragment = new CartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+
+        recyclerView = view.findViewById(R.id.cart_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        list = new ArrayList<>();
+
+        list.add(new CartModel(R.drawable.fries1, "Order 1", "3", "4.5"));
+        list.add(new CartModel(R.drawable.pizza1, "Order 2", "7", "4.5"));
+        list.add(new CartModel(R.drawable.pizza2, "Order 3", "10", "4.5"));
+        list.add(new CartModel(R.drawable.pizza3, "Order 4", "11", "4.7"));
+        list.add(new CartModel(R.drawable.pizza4, "Order 5", "12", "5"));
+
+        cartAdapter = new CartAdapter(list);
+        recyclerView.setAdapter(cartAdapter);
+
+        return view;
     }
 }
